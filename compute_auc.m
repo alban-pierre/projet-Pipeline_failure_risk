b3829e83f5a8eb7d56a1e6db564f8010d89a5a6d
alban-pierre <alban.pierre@ens.fr>
2017-02-11 23:31:25 +0100

    Fixed compute_auc

--- compute_auc.m
+++ compute_auc.m
@@ -3,7 +3,7 @@ function [score, auc14, auc15] = compute_auc(prediction, y)
     % Function that computes the score of an algorithm
 
     % 2014
-    [s, is] = sort(prediction(:,1));
+    [s, is] = sort(prediction(:,1), 'descend');
     truesorted = y(is,1);
     n = size(truesorted,1);
     cs = cumsum(truesorted(:,1),1);
@@ -17,7 +17,7 @@ function [score, auc14, auc15] = compute_auc(prediction, y)
     score(1,1) = mean(curve);
     
     %2015
-    [s, is] = sort(prediction(:,2));
+    [s, is] = sort(prediction(:,2), 'descend');
     truesorted = y(is,2);
     n = size(truesorted,1);
     cs = cumsum(truesorted(:,1),1);
