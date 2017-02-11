cb020b14f751535d4ee5e30f8dfe05e3d9354c5a
alban-pierre <alban.pierre@ens.fr>
2017-02-11 01:34:44 +0100

    Update compute_auc

--- compute_auc.m
+++ compute_auc.m
@@ -3,6 +3,7 @@ function [score, auc14, auc15] = compute_auc(prediction, y)
     % Function that computes the score of an algorithm
 
     % 2014
+	tt = time();
     [s, is] = sort(prediction(:,1));
     truesorted = y(is,1);
     n = size(truesorted,1);
@@ -12,11 +13,13 @@ function [score, auc14, auc15] = compute_auc(prediction, y)
     for i=1:n
         curve(1,i-sum(truesorted(1:i,:),1)) = sum(truesorted(1:i,:),1);
     end
-    curve = curve / max(curve,[],2);
+    curve = curve / max(max(curve,[],2),1);
     auc14 = curve;
     score(1,1) = mean(curve);
-	
+	time() - tt
+    
     % 2015
+	tt = time();
     [s, is] = sort(prediction(:,2));
     truesorted = y(is,2);
     n = size(truesorted,1);
@@ -26,9 +29,48 @@ function [score, auc14, auc15] = compute_auc(prediction, y)
     for i=1:n
         curve(1,i-sum(truesorted(1:i,:),1)) = sum(truesorted(1:i,:),1);
     end
-    curve = curve / max(curve,[],2);
+    curve = curve / max(max(curve,[],2),1);
     auc15 = curve;
     score(2,1) = mean(curve);
 
     score = [0.6,0.4]*score;
+	time() - tt
+
+    % 2014
+	tt = time();
+    [s, is] = sort(prediction(:,1));
+    truesorted = y(is,1);
+    n = size(truesorted,1);
+    cs = cumsum(truesorted(:,1),1);
+    ics = (1:size(cs,1))' - cumsum(truesorted(:,1),1);
+    s = n-sum(truesorted, 1);
+    curve = zeros(1,s);
+    for i=1:n
+        curve(1,ics(i,1)) = cs(i,1);
+    end
+    curve = curve / max(max(curve,[],2),1);
+	abs(sum(auc14 - curve))
+    auc14 = curve;
+    score(1,1) = mean(curve);
+	time() - tt
+
+	%2015
+	tt = time();
+    [s, is] = sort(prediction(:,2));
+    truesorted = y(is,2);
+    n = size(truesorted,1);
+    cs = cumsum(truesorted(:,1),1);
+    ics = (1:size(cs,1))' - cumsum(truesorted(:,1),1);
+    s = n-sum(truesorted, 1);
+    curve = zeros(1,s);
+    for i=1:n
+        curve(1,ics(i,1)) = cs(i,1);
+    end
+    curve = curve / max(max(curve,[],2),1);
+    abs(sum(auc15 - curve))
+	auc15 = curve;
+	score(2,1) = mean(curve);
+	score = [0.6,0.4]*score;
+	time() - tt
+	
 end
--- main.m
+++ main.m
@@ -13,20 +13,23 @@ nb_tests = 1;
 scores = zeros(2, nb_tests);
 
 for i=1:size(train_i,2)
+	tt = time();
 	trainx = datax(train_i{i},2:end);
 	trainy = datay(train_i{i},2:end);
 	testx = datax(test_i{i},2:end);
 	testy = datay(test_i{i},2:end);
-
+	%time() - tt
+	
 	% Main part, where all the prediction is done
-
+	tt = time();
 	prediction_train = randi(2,n-testsize,2)-1;
 	prediction_test = randi(2,testsize,2)-1;
-
+	%time() - tt
 	% End of the main part, here we only compute the error and plot it
-	
+	tt = time();
 	[scores(1,i), auc14{1,i}, auc15{1,i}] = compute_auc(prediction_train, trainy);
-	[scores(2,i), auc14{2,i}, auc15{1,i}] = compute_auc(prediction_test, testy);
+	[scores(2,i), auc14{2,i}, auc15{2,i}] = compute_auc(prediction_test, testy);
+	%time() - tt
 end
 
 mean(scores, 2)
