26442dacdf507f04b59eac3fecf17beee031d2d2
alban-pierre <alban.pierre@ens.fr>
2017-02-11 22:32:47 +0100

    Add an AUC plot function

--- main.m
+++ main.m
@@ -6,33 +6,33 @@ end
 
 n = size(datax,1);
 testsize = 1000;
-nb_tests = 1;
+nb_tests = 100;
 
 [train_i, test_i] = train_test_sets(n, testsize, nb_tests);
 
 scores = zeros(2, nb_tests);
+clear auc14;
+clear auc15;
 
 for i=1:size(train_i,2)
-	%tt = time();
-	trainx = datax(train_i{i},2:end);
-	trainy = datay(train_i{i},2:end);
-	testx = datax(test_i{i},2:end);
-	testy = datay(test_i{i},2:end);
-	%time() - tt
-	
-	% Main part, where all the prediction is done
-	%tt = time();
-	prediction_train = randi(2,n-testsize,2)-1;
-	prediction_test = randi(2,testsize,2)-1;
-	%time() - tt
-	% End of the main part, here we only compute the error and plot it
-	%tt = time();
-	[scores(1,i), auc14{1,i}, auc15{1,i}] = compute_auc(prediction_train, trainy);
-	[scores(2,i), auc14{2,i}, auc15{2,i}] = compute_auc(prediction_test, testy);
-	%time() - tt
+    %tt = time();
+    trainx = datax(train_i{i},2:end);
+    trainy = datay(train_i{i},2:end);
+    testx = datax(test_i{i},2:end);
+    testy = datay(test_i{i},2:end);
+    %time() - tt
+    
+    % Main part, where all the prediction is done
+    %tt = time();
+    prediction_train = randi(2,n-testsize,2)-1;
+    prediction_test = randi(2,testsize,2)-1;
+    %time() - tt
+    % End of the main part, here we only compute the error and plot it
+    %tt = time();
+    [scores(1,i), auc14{1,i}, auc15{1,i}] = compute_auc(prediction_train, trainy);
+    [scores(2,i), auc14{2,i}, auc15{2,i}] = compute_auc(prediction_test, testy);
+    %time() - tt
 end
 
 mean(scores, 2)
-
-
-
+plot_auc(auc14, auc15);
---
+++ plot_auc.m
@@ -0,0 +1,64 @@
+function plot_auc(auc14, auc15)
+
+    % Plots the train and test auc of 2014 and 2015
+
+    n = max(size(auc14,2), size(auc15,2));
+    
+    colors = rand(max(n,6), 3);
+    colors = colors - repmat(max(0,sum(colors,2)-2)/2,1,3);
+    colors(1,:) = [0,0,0];
+    colors(2,:) = [1,0,0];
+    colors(3,:) = [0,0,1];
+    colors(4,:) = [0,1,0];
+    colors(5,:) = [1,0,1];
+    colors(6,:) = [0,1,1];
+
+
+    figure; hold on;
+    for i=1:size(auc14,2)
+        if size(auc14{1,i} > 0)
+            c = auc14{1,i};
+            plot((0:size(c,2)-1)/(size(c,2)-1), c, '-', 'color', colors(i,:));
+        end
+    end
+    title('Training AUC for 2014');
+    xlabel('False positive');
+    ylabel('True positive');
+
+    
+    figure; hold on;
+    for i=1:size(auc14,2)
+        if size(auc14{2,i} > 0)
+            c = auc14{2,i};
+            plot((0:size(c,2)-1)/(size(c,2)-1), c, '-', 'color', colors(i,:));
+        end
+    end
+    title('Testing AUC for 2014');
+    xlabel('False positive');
+    ylabel('True positive');
+
+
+    figure; hold on;
+    for i=1:size(auc15,2)
+        if size(auc15{1,i} > 0)
+            c = auc15{1,i};
+            plot((0:size(c,2)-1)/(size(c,2)-1), c, '-', 'color', colors(i,:));
+        end
+    end
+    title('Training AUC for 2015');
+    xlabel('False positive');
+    ylabel('True positive');
+
+    
+    figure; hold on;
+    for i=1:size(auc15,2)
+        if size(auc15{2,i} > 0)
+            c = auc15{2,i};
+            plot((0:size(c,2)-1)/(size(c,2)-1), c, '-', 'color', colors(i,:));
+        end
+    end
+    title('Testing AUC for 2015');
+    xlabel('False positive');
+    ylabel('True positive');
+    
+end
