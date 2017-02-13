9766baea9aacb7e1c5fdfb88dfe226672fdf5eb9
alban-pierre <alban.pierre@ens.fr>
2017-02-13 20:34:09 +0100

    Print time taken in main

--- main.m
+++ main.m
@@ -3,18 +3,18 @@
 submit_file = 0; % Set it to 1 to produce a result file to submit on data challenge, 0 otherwise
 
 % Loading datasets and formatting the data
-if ((exist('datax') ~= 1) || (exist('datay') ~= 1))
-    [datax, datay] = load_data();
+if ((exist('datainitx') ~= 1) || (exist('datainity') ~= 1))
+    [datainitx, datainity] = load_data();
 end
 if (submit_file && (exist('datas') ~= 1))
     datas = load_data(1);
 end
 
 % Options (useless if we are submitting a file)
-n = size(datax,1);
-testsize = 5000;%6476; % The testsize
-trainsize = 10000;%n-testsize;
-nb_tests = 2; % The number of tests
+n = size(datainitx,1);
+testsize = 6476; % The testsize
+trainsize = n-testsize;
+nb_tests = 10; % The number of tests
 
 % Defining train and test sets
 [train_i, test_i] = random_train_test_sets(trainsize, testsize, nb_tests);
@@ -23,25 +23,34 @@ scores = zeros(2, nb_tests);
 clear auc14;
 clear auc15;
 
+% Modification of the data representation
+tt = time();
+datax = datainitx(:,2:end);
+datay = datainity(:,2:end);
+datax = remove_constant_columns(add_power2_columns(datax, ones(size(datax,2))));
+
+fprintf(2, 'The data representation transformation took %f seconds\n', time() - tt);
+
 if (submit_file) % In the particular case of submitting
     testsize = 9713;
     nb_tests = 1;
-    trainx = datax(:,2:end);
-    trainy = datay(:,2:end);
+    trainx = datainitx(:,2:end);
+    trainy = datainity(:,2:end);
     testx = datas(:,2:end);
     train_i = 0;
     scores = 0;
 end
 
 % For each train set
+tt = time();
 for i=1:size(train_i,2)
 
     % create train and test sets
     if (~submit_file)
-        trainx = datax(train_i{i},2:end);
-        trainy = datay(train_i{i},2:end);
-        testx = datax(test_i{i},2:end);
-        testy = datay(test_i{i},2:end);
+        trainx = datax(train_i{i},:);
+        trainy = datay(train_i{i},:);
+        testx = datax(test_i{i},:);
+        testy = datay(test_i{i},:);
     end
     
     % Main part, where all the prediction is done
@@ -74,6 +83,7 @@ for i=1:size(train_i,2)
         auc15{2,i} = auc{2};
     end
 end
+fprintf(2, 'The predictions took %f seconds\n', time() - tt);
 
 % Print scores and plot auc curves
 mean(scores, 2)
